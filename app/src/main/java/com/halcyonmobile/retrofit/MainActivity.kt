ad5393d39de0e77a6e5839b5d2a3d5110c3646e0
Gergely Hegedus <gergo@halcyonmobile.com>
2019-09-09 15:43:29 +0300

    Add example request in the android app

--- app/src/main/java/com/halcyonmobile/retrofit/MainActivity.kt
+++ app/src/main/java/com/halcyonmobile/retrofit/MainActivity.kt
@@ -2,6 +2,14 @@ package com.halcyonmobile.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.halcyonmobile.errorparsing.ErrorWrappingAndParserCallAdapterFactory
+import com.halcyonmobile.errorparsing.NetworkException
+import com.halcyonmobile.errorparsing.WrapIntoNetworkException
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.*
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +17,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val service = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().build()))
+            .addCallAdapterFactory(ErrorWrappingAndParserCallAdapterFactory())
+            .baseUrl("https://www.google.com/")
+            .build()
+            .create(Service::class.java)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                service.networkErrorWrapping()
+            } catch (networkException: NetworkException){
+                networkException.printStackTrace()
+                System.err.println("error caught")
+            }
+        }
+    }
+
+    interface Service {
+
+        @WrapIntoNetworkException
+        @GET("alma")
+        suspend fun networkErrorWrapping(): Unit?
     }
 }
