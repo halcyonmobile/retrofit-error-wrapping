4c9c16049e1c1a0a8aa3f9454974b69cad41466d
Gergely Hegedus <gergo@halcyonmobile.com>
2019-10-28 18:44:01 +0200

    Make NetworkException open

--- error-parsing-exceptions/src/main/java/com/halcyonmobile/errorparsing/NetworkException.kt
+++ error-parsing-exceptions/src/main/java/com/halcyonmobile/errorparsing/NetworkException.kt
@@ -4,4 +4,4 @@ package com.halcyonmobile.errorparsing
  * General Network Exception, it't contains the errorBody, if there were any in [errorBody] as string
  * and a parsed errorBody in [parsedError] if it could be parsed.
  */
-class NetworkException constructor(throwable: Throwable?, val parsedError: Any?, val errorBody: String?) : RuntimeException(throwable)
+open class NetworkException constructor(throwable: Throwable?, val parsedError: Any?, val errorBody: String?) : RuntimeException(throwable)
